577fda144f91ec985af0f7854550a0c84fb38f94
Patrick <inetreg@pat-schmitz.de>
2024-08-07 15:09:16 +0200

    Updated deploy.py to the newest version (compatible with Python 3.*)

--- helper_tools/Python/liquibase/deploy.py
+++ helper_tools/Python/liquibase/deploy.py
@@ -5,28 +5,45 @@
 # on OS=linux
 import os
 import sys
-import ConfigParser
+if (int(sys.version_info[0]) >= 3):
+    import configparser as ConfigParser
+else:
+    import ConfigParser
 import subprocess
 import datetime
 
 CONFIGFOLDER = ""
 DATETIMENOW = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
+DEBUG = False
+if os.getenv("LQB_DEBUG", "No")!="No":
+    DEBUG = True
+
 # Command Line Parameters will be detected here
 def cli_params():
     global ENVKEY
     global CONFIGFOLDER
-    print sys.argv
+    print(sys.argv)
     if len(sys.argv) < 2:
-        print "You must call me like:"
-        print "   deploy.py <Environment Key (e.g. DEV)>"
+        print("You must call me like:")
+        print("   deploy.py <Environment Key (e.g. DEV)>")
         raise Exception("Call parameter missing!")
         sys.exit(1)
     ENVKEY = sys.argv[1]
     try:
         CONFIGFOLDER = sys.argv[2]
     except:
-        print "CONFIGFOLDER not given. Using default"
+        print("CONFIGFOLDER not given. Using default")
+        
+    try:
+        param_DEBUG = sys.argv[3]
+        if param_DEBUG!="":
+            DEBUG = True
+    except:
+        pass
+
+if DEBUG:
+    print("DEBUG Mode = ON")
 
 def ConfigSectionMap(section):
     dict1 = {}
@@ -51,21 +68,34 @@ def writeOutputLog(logpath, msg):
 	myFile.close() 
     
 def printCurrentEnvSettings(prop, value):
-    print prop + ": "  + value
+    print(prop + ": "  + value)
     writeOutputLog(logfilepath, prop + ": "  + value + "\n")
 	
 def getFilePathRelativeScriptPath(filepath):
     return os.path.abspath(os.path.join(dirPath, filepath))
 
-debug=0
-cli_params()
+def getAdditionalLiquibaseParameter(Config, section='additional_liquibase_parameter'):
+    additional_liquibase_parameter_found = False
+    params = dict()
+    try:
+        params = dict(ConfigNoEnv.items(section))
+        additional_liquibase_parameter_found = True
+    except:
+        pass
+    if params==dict():
+        additional_liquibase_parameter_found = False
+    return additional_liquibase_parameter_found, params
 
-if CONFIGFOLDER == "":
-    CONFIGFOLDER = "D:\\Entwicklung\\_Privat\\meta_grid\\DWH_Meta_wrkCpy_DEV\\dwh_meta_v2\\database_model\\liquibase\\"
+# if (int(sys.version_info[0]) < 3):
+#     print("You need Python 3 to run this script!")
+#     sys.exit(1)
+
+
+cli_params()
 
 
 if (os.path.isfile(getConfigFilePath(CONFIGFOLDER, ENVKEY))):
-    print "Config file found: " + getConfigFilePath(CONFIGFOLDER, ENVKEY)
+    print("Config file found: " + getConfigFilePath(CONFIGFOLDER, ENVKEY))
 else:
     raise Exception("Config file not found!")
     sys.exit(1)
@@ -78,10 +108,16 @@ realPath = os.path.realpath(currentFile)  # /home/user/test/my_script.py
 dirPath = os.path.dirname(realPath)  # /home/user/test
 dirName = os.path.basename(dirPath) # test
 
-
-Config = ConfigParser.SafeConfigParser()
+if (int(sys.version_info[0]) >= 3):
+    Config = ConfigParser.ConfigParser(os.environ)
+    ConfigNoEnv = ConfigParser.ConfigParser(interpolation=None)
+else:
+    Config = ConfigParser.SafeConfigParser(os.environ)
+    ConfigNoEnv = ConfigParser.SafeConfigParser(interpolation=None)
 # Config._interpolation = ConfigParser.ExtendedInterpolation()
 Config.read(getConfigFilePath(CONFIGFOLDER, ENVKEY))
+ConfigNoEnv.read(getConfigFilePath(CONFIGFOLDER, ENVKEY))
+
 
 # Get the settings
 liquibasePathExe = Config.get("liquibase", "liquibasePathExe")
@@ -89,10 +125,19 @@ liquibaseDriver = Config.get("liquibase", "liquibaseDriver")
 liquibaseChangeLogFile = Config.get("liquibase", "liquibaseChangeLogFile")
 liquibaseAction = Config.get("liquibase", "liquibaseAction")
 liquibaseActionValue = Config.get("liquibase", "liquibaseActionValue")
-liquibasePropertiesFile = Config.get("liquibase", "liquibasePropertiesFile")
-liquibasePromptForDBPassword = Config.get("liquibase", "liquibasePromptForDBPassword")
+
+# {... rate_me
+liquibasePropertiesFile = Config.get("liquibase", "liquibasePropertiesFile", fallback='')
+liquibasePromptForDBPassword = Config.get("liquibase", "liquibasePromptForDBPassword", fallback='0')
+# rate_me ...}
+
 sqliteBin = Config.get("sqlite", "sqliteBin")
 dbpath = Config.get("environment", "dbpath")
+dbuser = Config.get("environment", "dbuser", fallback='')
+dbpassword = Config.get("environment", "dbpassword", fallback='')
+liquibaseDatabaseChangeLogTableName=Config.get("liquibase", "liquibaseDatabaseChangeLogTableName", fallback='DATABASECHANGELOG')
+liquibaseDatabaseChangeLogLockTableName=Config.get("liquibase", "liquibaseDatabaseChangeLogLockTableName", fallback='DATABASECHANGELOGLOCK')
+
 liquibaseDriverUrlprefix = Config.get("environment", "liquibaseDriverUrlprefix")
 comment = Config.get("other", "comment")
 
@@ -113,11 +158,19 @@ printCurrentEnvSettings("liquibaseChangeLogFile", liquibaseChangeLogFile)
 printCurrentEnvSettings("Absolute liquibaseChangeLogFile", getFilePathRelativeScriptPath(liquibaseChangeLogFile))
 printCurrentEnvSettings("liquibaseAction", liquibaseAction)
 printCurrentEnvSettings("liquibaseActionValue", liquibaseActionValue)
+
+# {... rate_me
 printCurrentEnvSettings("liquibasePropertiesFile", liquibasePropertiesFile)
 printCurrentEnvSettings("liquibasePromptForDBPassword", liquibasePromptForDBPassword)
 printCurrentEnvSettings("Absolute liquibasePropertiesFile", getFilePathRelativeScriptPath(liquibasePropertiesFile))
+# rate_me ...}
+
 printCurrentEnvSettings("sqliteBin", sqliteBin)
 printCurrentEnvSettings("dbpath", dbpath)
+printCurrentEnvSettings("dbuser", dbuser)
+# printCurrentEnvSettings("dbpassword", dbpassword)
+printCurrentEnvSettings("liquibaseDatabaseChangeLogTableName", liquibaseDatabaseChangeLogTableName)
+printCurrentEnvSettings("liquibaseDatabaseChangeLogLockTableName", liquibaseDatabaseChangeLogLockTableName)
 printCurrentEnvSettings("Absolute dbpath", getFilePathRelativeScriptPath(dbpath))
 printCurrentEnvSettings("liquibaseDriverUrlprefix", liquibaseDriverUrlprefix)
 printCurrentEnvSettings("comment", comment)
@@ -128,42 +181,83 @@ if liquibaseDriver == "org.sqlite.JDBC":
         raise Exception("SQLite File not found!")
         sys.exit(1)
 
-password=""
+# {... rate_me
 if liquibasePromptForDBPassword == "1":
     from getpass import getpass
-    password = getpass()
+    dbpassword = getpass()
+# rate_me ...}
 
-# exec liquibase
+
+
+# Liquibase parameter
+liquibase_parameter=[]
+liquibase_parameter.append(liquibasePathExe)
+liquibase_parameter.append("--driver=" + liquibaseDriver)
+liquibase_parameter.append("--databaseChangeLogTableName=" + liquibaseDatabaseChangeLogTableName)
+liquibase_parameter.append("--databaseChangeLogLockTableName=" + liquibaseDatabaseChangeLogLockTableName)
+liquibase_parameter.append("--changeLogFile=" + getFilePathRelativeScriptPath(liquibaseChangeLogFile))
+liquibase_parameter.append("--url=" + liquibaseDriverUrlprefix)
+# handling for databases with user and password
+if (dbuser != ""):
+    liquibase_parameter.append("--username=" + dbuser)
+if (dbpassword != ""):
+    liquibase_parameter.append("--password=" + dbpassword)
+
+# {... rate_me
+if (liquibasePropertiesFile != ""):
+    liquibase_parameter.append("--defaultsFile=" + getFilePathRelativeScriptPath(liquibasePropertiesFile))
+# rate_me ...}
+
+if DEBUG:
+    liquibase_parameter.append("--logLevel=" + "FINE")
+liquibase_parameter.append(liquibaseAction)
+
 if liquibaseActionValue != "":
-    #subprocess.call([liquibasePathExe, "--driver=" + liquibaseDriver, "--changeLogFile=" + getFilePathRelativeScriptPath(liquibaseChangeLogFile), "--url=" + liquibaseDriverUrlprefix , liquibaseAction, liquibaseActionValue])
+    liquibase_parameter.append(liquibaseActionValue)
+
+addi_params_found, addi_params = getAdditionalLiquibaseParameter(Config=ConfigNoEnv)
+if addi_params_found:
+    for parameter_key,parameter_value in addi_params.items():
+        liquibase_parameter.append(f"-D{parameter_key}={parameter_value}")
+
+# exec liquibase command
+try:
     try:
-        try:
-            output = subprocess.check_output([liquibasePathExe, "--password=" + password, "--defaultsFile=" + getFilePathRelativeScriptPath(liquibasePropertiesFile), "--driver=" + liquibaseDriver, "--changeLogFile=" + getFilePathRelativeScriptPath(liquibaseChangeLogFile), "--url=" + liquibaseDriverUrlprefix , liquibaseAction, liquibaseActionValue], stderr=subprocess.STDOUT).decode()
-        except:
-            output = subprocess.check_output([liquibasePathExe, "--password=" + password, "--defaultsFile=" + getFilePathRelativeScriptPath(liquibasePropertiesFile), "--driver=" + liquibaseDriver, "--changeLogFile=" + getFilePathRelativeScriptPath(liquibaseChangeLogFile), "--url=" + liquibaseDriverUrlprefix , liquibaseAction, liquibaseActionValue], stderr=subprocess.STDOUT)     
-        success = True
-    except subprocess.CalledProcessError as e:
-        output = ""
-        try:
-            output = e.output.decode()
-        except:
-            output = e.output
-        success = False
-    writeOutputLog(logfilepath, output)
-    print output
+        output = subprocess.check_output(liquibase_parameter, stderr=subprocess.STDOUT).decode()
+    except:
+        output = subprocess.check_output(liquibase_parameter, stderr=subprocess.STDOUT)     
+    success = True
+except subprocess.CalledProcessError as e:
+    output = ""
+    try:
+        output = e.output.decode()
+    except:
+        output = e.output
+    success = False
+writeOutputLog(logfilepath, output)
+print(output)
+
+if DEBUG:
+    print(f"""LiquiBase command:
+    {" ".join(liquibase_parameter)}
+    """)
+
+    print(f"""All config items from section 'additional_liquibase_parameter':
+    Parameter found: {addi_params_found}
+    Parameters: {addi_params}""")
+
+if "Cannot find database driver: org.postgresql.Driver" in output:
+    from urllib.request import urlretrieve
+    jdbcdriver="postgresql-42.7.1.jar"
+    url = "https://jdbc.postgresql.org/download/" + jdbcdriver
+    download_path_temp = liquibasePathExe.split(os.sep)
+    download_path_temp[-1]='lib'
+    download_path_temp.append(jdbcdriver)
+    download_path=os.sep.join(download_path_temp)
+    urlretrieve(url, download_path)
+    print(f"Driver downloaded from {url} to {download_path}. Please try again." )
+    
+if not success:
+    sys.exit(-1)
 else:
-    #subprocess.call([liquibasePathExe, "--driver=" + liquibaseDriver, "--changeLogFile=" + getFilePathRelativeScriptPath(liquibaseChangeLogFile), "--url=" + liquibaseDriverUrlprefix , liquibaseAction])        
-    try:
-        try:
-            output = subprocess.check_output([liquibasePathExe, "--password=" + password, "--defaultsFile=" + getFilePathRelativeScriptPath(liquibasePropertiesFile), "--driver=" + liquibaseDriver, "--changeLogFile=" + getFilePathRelativeScriptPath(liquibaseChangeLogFile), "--url=" + liquibaseDriverUrlprefix , liquibaseAction], stderr=subprocess.STDOUT).decode()
-        except:
-            output = subprocess.check_output([liquibasePathExe, "--password=" + password, "--defaultsFile=" + getFilePathRelativeScriptPath(liquibasePropertiesFile), "--driver=" + liquibaseDriver, "--changeLogFile=" + getFilePathRelativeScriptPath(liquibaseChangeLogFile), "--url=" + liquibaseDriverUrlprefix , liquibaseAction], stderr=subprocess.STDOUT)
-        success = True 
-    except subprocess.CalledProcessError as e:
-        try:
-            output = e.output.decode()
-        except:
-            output = e.output
-        success = False
-    writeOutputLog(logfilepath, output)
-    print output
+    sys.exit(0)
